636814dc23ebaa1c2069408932253ee42788e142
nirojneupane5 <nirojneupane5@gmail.com>
2024-05-26 20:53:10 +0545

    Form created using shadcn for adding course from the client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="text-red-500">
+      <Button>Hello</Button>
+    </div>
+  );
 }
