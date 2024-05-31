539cc31b391e5e9b2ceed0a19cb64eb12165e8ba
nirojneupane5 <nirojneupane5@gmail.com>
2024-05-31 14:18:49 +0545

    Course description validation changed to atleast 20 character and card layout design chnage

--- src/course/DisplayCourse.tsx
+++ src/course/DisplayCourse.tsx
@@ -20,9 +20,17 @@ export default function DisplayCourse({ courseData }: CourseProps) {
                 height={500}
               />
             </div>
-            <h1>{info.course_name}</h1>
-            <h3>{info.price}</h3>
-            <p>{info.course_desc}</p>
+            <div className="px-2 py-2">
+              <h1 className="text-2xl font-bold text-black">
+                Course Name:{info.course_name}
+              </h1>
+              <h3 className="text-xl font-bold text-red-400">
+                Price: Rs {info.price}
+              </h3>
+              <p className="text-justify line-clamp-3 font-light text-black">
+                {info.course_desc}
+              </p>
+            </div>
           </div>
         ))}
       </div>
