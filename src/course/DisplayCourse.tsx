739756797b6ea54dd63cba192bc9fc04fb7342eb
nirojneupane5 <nirojneupane5@gmail.com>
2024-05-31 13:51:13 +0545

    Card created with responsiveness fro display

--- src/course/DisplayCourse.tsx
+++ src/course/DisplayCourse.tsx
@@ -7,9 +7,22 @@ type CourseProps = {
 export default function DisplayCourse({ courseData }: CourseProps) {
   return (
     <section>
-      {courseData.map((info, index) => (
-        <h1 key={index}>{info.course_name}</h1>
-      ))}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 pt-2">
+        {courseData.map((info, index) => (
+          <div key={index} className="shadow-md rounded-md text-center">
+            <div className="overflow-hidden">
+              <img
+                src={`${process.env.NEXT_PUBLIC_COURSE_IMAGE}/${info.course_image}`}
+                alt="Course Image"
+                className="object-cover h-[200px] w-full hover:scale-150 duration-300"
+              />
+            </div>
+            <h1>{info.course_name}</h1>
+            <h3>{info.price}</h3>
+            <p>{info.course_desc}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
