d7894cfb3c9d0c2d5ba61ed3395960106c0e9c14
nirojneupane5 <nirojneupane5@gmail.com>
2024-05-31 17:03:32 +0545

    Backgorund color changed when hovering the card

--- src/api/course-api.ts
+++ src/api/course-api.ts
@@ -10,7 +10,6 @@ export type TCourse={
 //Create a course
 export const createCourse=async(newsValue:FormData)=>{
     const response=await axios.post(`${process.env.NEXT_PUBLIC_URL}/course`,newsValue);
-    console.log(response.data);
     return response.data
 }
 
