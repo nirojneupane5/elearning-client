af72877825f9905550c83587ab9b88e533baf9d1
nirojneupane5 <nirojneupane5@gmail.com>
2024-05-31 17:48:55 +0545

    TCourseSingle type created, API for the course created and and dispaly the single course name

--- src/api/course-api.ts
+++ src/api/course-api.ts
@@ -8,15 +8,28 @@ export type TCourse={
     course_image:string
 }[]
 
+export type TCourseSingle={
+    _id:string;
+    course_name:string;
+    course_desc:string;
+    price:string;
+    course_image:string
+}
+
 //Create a course
 export const createCourse=async(newsValue:FormData)=>{
     const response=await axios.post(`${process.env.NEXT_PUBLIC_URL}/course`,newsValue);
     return response.data
 }
 
-
 //Display Course
 export const displayCourse=async <T>():Promise<T>=>{
     const response=await axios.get<T>(`${process.env.NEXT_PUBLIC_URL}/course`);
+    return response.data
+}
+
+//Display Single course
+export const displaySingleCourse=async<T>(id:string):Promise<T>=>{
+    const response=await axios.get<T>(`${process.env.NEXT_PUBLIC_URL}/course/${id}`);
     return response.data
 }
